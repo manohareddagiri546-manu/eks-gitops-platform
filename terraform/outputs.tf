384f0e90845717daafeda38bb2c21dcdfad7dfae
manohareddagiri546-manu <manohareddagiri546@gmail.com>
2026-07-14 07:03:14 -0400

    Update outputs.tf
    Fix Terraform output syntax

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,20 @@
-output "cluster_name" { value = module.eks.cluster_name }
-output "cluster_endpoint" { value = module.eks.cluster_endpoint
-  sensitive = true }
-output "oidc_provider_arn" { value = module.eks.oidc_provider_arn }
-output "ecr_repository_url" { value = aws_ecr_repository.app.repository_url }
-output "github_actions_role_arn" { value = aws_iam_role.github_actions.arn }
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value     = module.eks.cluster_endpoint
+  sensitive = true
+}
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
